33305a933f9efedf7a00102fe8f9e523f31f9a12
OEG <oeg8168@gmail.com>
2017-03-26 21:18:23 +0800

    343, 94

---
+++ src/94.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        self.ans = []
+        self.dfsInorder(root)
+        return self.ans
+
+    def dfsInorder(self, node):
+        if node.left is not None:
+            self.dfsInorder(node.left)
+
+        self.ans.append(node.val)
+
+        if node.right is not None:
+            self.dfsInorder(node.right)
+
+    # Same logic but different style
+    # Ref:
+    # https://discuss.leetcode.com/topic/21350/python-recursive-and-iterative-solutions
+    def inorderTraversal_anotherSolution(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        self.ans = []
+        self.dfsInorder(root)
+        return self.ans
+
+    def dfsInorder_anotherSolution(self, node):
+        if node:
+            self.dfsInorder(node.left)
+            self.ans.append(node.val)
+            self.dfsInorder(node.right)
